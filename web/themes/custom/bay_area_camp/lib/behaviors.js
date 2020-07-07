56438bc65f6568fbaafe9eb6afc8b08ecd177759
mmiller <miller.2676@osu.edu>
2020-07-07 12:29:51 -0700

    Replace table checkmarks with svg

--- web/themes/custom/bay_area_camp/lib/behaviors.js
+++ web/themes/custom/bay_area_camp/lib/behaviors.js
@@ -1,19 +1,20 @@
-(function(Drupal, $) {
+(function (Drupal, $) {
   Drupal.behaviors.foundation = {
-    attach: function(context) {
+    attach: function (context) {
       $(context).foundation();
     },
   };
 
-  Drupal.behaviors.sponsorship= {
-    attach: function(context) {
-      $(context).find('.sponsorship-table td').each(function() {
-        if($(this).is(':contains("•")')) {
-          $(this).text('');
-          $(this).addClass('table-bullet');
-        }
-      })
+  Drupal.behaviors.sponsorship = {
+    attach: function (context) {
+      $(context)
+        .find(".sponsorship-table td")
+        .each(function () {
+          if ($(this).is(':contains("•")')) {
+            $(this).html("<span class='visually-hidden'>yes</span>");
+            $(this).addClass("table-bullet");
+          }
+        });
     },
   };
-
 })(Drupal, jQuery);
